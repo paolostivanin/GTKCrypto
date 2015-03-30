d3a938f1113bd4c7ab3230b526d314d59fb78c3b
Paolo Stivanin <info@paolostivanin.com>
2015-03-30 19:03:44 +0200

    small fixes

--- src/get_file_size.c
+++ src/get_file_size.c
@@ -15,10 +15,10 @@ get_file_size (const gchar *file_path)
 	goffset file_size;
 
 	file = g_file_new_for_path (file_path);
-	info = g_file_query_info (file, attributes, flags, cancellable, &error);
+	info = g_file_query_info (G_FILE(file), attributes, flags, cancellable, &error);
 	file_size = g_file_info_get_size (info);
 
-	g_object_unref(file);
+	g_object_unref (file);
 	
 	return file_size;
 }
