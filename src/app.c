c4492d25d6449b8c1ec2a26d9ec7f1a500af1ddc
Paolo Stivanin <info@paolostivanin.com>
2018-02-25 14:39:32 +0100

    use ui file for main window
    - remove unneeded file

--- src/app.c
+++ src/app.c
@@ -3,14 +3,17 @@
 #include "main.h"
 #include "gtkcrypto.h"
 
-static GtkWidget *create_main_window (GtkApplication *app);
+static GtkWidget *get_main_window (GtkBuilder *builder);
 
 
 void
 activate (GtkApplication *app,
           gpointer        user_data __attribute__((unused)))
 {
-    GtkWidget *main_window = create_main_window (app);
+    // TODO change me
+    GtkBuilder *builder = gtk_builder_new_from_file ("../src/ui/widgets.ui");
+
+    GtkWidget *main_window = get_main_window (builder);
     gtk_application_add_window (GTK_APPLICATION (app), GTK_WINDOW (main_window));
 
     if (!gcry_check_version (GCRYPT_MIN_VERSION)) {
@@ -18,42 +21,37 @@ activate (GtkApplication *app,
         return;
     }
 
-    gcry_control (GCRYCTL_INIT_SECMEM, SECURE_MEMORY_POOL_SIZE, 0);
+    if (gcry_control (GCRYCTL_INIT_SECMEM, SECURE_MEMORY_POOL_SIZE, 0)) {
+        show_message_dialog (main_window, "Couldn't initialize secure memory.\n", GTK_MESSAGE_ERROR);
+        g_application_quit (G_APPLICATION (app));
+        return;
+    }
     gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
 
-    add_boxes_and_grid (main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "encfile_btn")), "clicked", G_CALLBACK (encrypt_files_cb), main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "decfile_btn")), "clicked", G_CALLBACK (decrypt_files_cb), main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "sigfile_btn")), "clicked", G_CALLBACK (sign_file_cb), main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "versig_btn")), "clicked", G_CALLBACK (verify_signature_cb), main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "chash_btn")), "clicked", G_CALLBACK (compute_hash_cb), main_window);
+    g_signal_connect (GTK_WIDGET (gtk_builder_get_object (builder, "cmphash_btn")), "clicked", G_CALLBACK (compare_files_hash_cb), main_window);
+
+    g_object_unref (builder);
 
     gtk_widget_show_all (main_window);
 }
 
 
 static GtkWidget *
-create_main_window (GtkApplication *app)
+get_main_window (GtkBuilder *builder)
 {
-    GtkWidget *window = gtk_application_window_new (app);
-    gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
-    gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
+    GtkWidget *window = GTK_WIDGET (gtk_builder_get_object(builder, "mainwin"));
 
-    gtk_window_set_icon_name (GTK_WINDOW (window), "gtkcrypto");
-
-    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-
-    gtk_widget_set_size_request (GTK_WIDGET (window), 475, 360);
-
-    gchar *header_bar_text = g_malloc (strlen (APP_NAME ) + 1 + strlen (APP_VERSION) + 1);
-    g_snprintf (header_bar_text, strlen (APP_NAME) + 1 + strlen (APP_VERSION) + 1,
-                "%s %s", APP_NAME, APP_VERSION);
+    gchar *header_bar_text = g_malloc (strlen (APP_NAME) + strlen (APP_VERSION) + 2);
+    g_snprintf (header_bar_text, strlen (APP_NAME) + strlen (APP_VERSION) + 2, "%s %s", APP_NAME, APP_VERSION);
     header_bar_text[strlen(header_bar_text)] = '\0';
 
-    GtkWidget *header_bar = gtk_header_bar_new ();
-    gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header_bar), TRUE);
+    GtkWidget *header_bar = (GTK_WIDGET (gtk_builder_get_object(builder, "main_hb")));
     gtk_header_bar_set_title (GTK_HEADER_BAR (header_bar), header_bar_text);
-    gtk_header_bar_set_has_subtitle (GTK_HEADER_BAR (header_bar), FALSE);
-
-    GtkWidget *box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_style_context_add_class (gtk_widget_get_style_context (box), "linked");
-
-    gtk_window_set_titlebar (GTK_WINDOW (window), header_bar);
 
     return window;
 }
